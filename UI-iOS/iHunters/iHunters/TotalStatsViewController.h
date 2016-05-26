2884c164f54cde2926c71de86309e72ab1f95364
Nick Nevsky <nik.misima@gmail.com>
2016-05-26 14:11:22 +0300

    Created total stats view controller.

---
+++ UI-iOS/iHunters/iHunters/TotalStatsViewController.h
@@ -0,0 +1,13 @@
+//
+//  TotalStatsViewController.h
+//  iHunters
+//
+//  Created by Nick Nevsky on 26.05.16.
+//  Copyright © 2016 iHuntersteam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TotalStatsViewController : UIViewController
+
+@end
