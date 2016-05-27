0d43bda54d027583b0ef0f849afd0dca776653d3
Aleksej Smaga <tattoo1mag@gmail.com>
2016-05-28 02:35:57 +0800

    change format string for db cursor execute

--- crawler/crawler_python/db_connect.py
+++ crawler/crawler_python/db_connect.py
@@ -24,59 +24,52 @@ def err(e):
 
 class CrawlerSitesConnector:
 
-    def get(self, *ids):
-        format_strings = ','.join(['%s'] * len(ids))
+    def get(self, ids):
         try:
             CURSOR.execute('''
-                SELECT id, url FROM pages WHERE site_id IN (%s)
-                ''' % format_strings, ids)
+                SELECT id, url FROM pages WHERE site_id IN ({})
+                '''.format(ids))
             return {k: v for k, v in CURSOR.fetchall()}
         except MySQLError as e:
             print(err(e))
 
-    def save(self, url, site_id):
+    def save(self, url, id):
         try:
             CURSOR.execute('''
                 INSERT INTO pages(url, site_id)
-                VALUES(%s, %s)
-                ''', (url, site_id))
-        except MySQLError as e:
-            print(err(e))
-
-    def update(self, id):
-        try:
-            CURSOR.execute('''
-                UPDATE pages SET last_scan_date=NOW()
-                WHERE id=%s
-                ''', (id))
+                VALUES({}, {})
+                '''.format(url, id))
         except MySQLError as e:
             print(err(e))
 
 
 class CrawlerPersonPageRankConnector:
 
-    def save(self, person_id, page_id, rank):
-        try:
-            CURSOR.execute('''
-                INSERT INTO person_page_rank(person_id, page_id, rank)
-                VALUES(%s, %s, %s)
-                ''', (person_id, page_id, rank))
-        except MySQLError as e:
-            print(err(e))
+    def save(self, dict_ranks):
+        for page_id, v in dict_ranks.items():
+            for person_id, rank in v.items():
+                try:
+                    CURSOR.execute('''
+                        INSERT INTO person_page_rank(person_id, page_id, rank)
+                        VALUES({}, {}, {})
+                        '''.format(person_id, page_id, rank))
+
+                except MySQLError as e:
+                    print(err(e))
 
 
 class CrawlerPersonsConnector:
 
-    def get(self, *ids):
+    def get(self, ids):
+        print(ids)
         try:
-            format_strings = ','.join(['%s'] * len(ids))
             CURSOR.execute('''
-                SELECT id, name FROM persons WHERE id IN (%s)
-                ''' % format_strings, ids)
+                SELECT id, name FROM persons WHERE id IN ({})
+                '''.format(ids))
             persons = CURSOR.fetchall()
             CURSOR.execute('''
-                SELECT person_id, name FROM keywords WHERE person_id IN (%s)
-            ''' % format_strings, ids)
+                SELECT person_id, name FROM keywords WHERE person_id IN ({})
+            '''.format(ids))
             keywords = list(CURSOR.fetchall())
             keywords.extend(persons)
             persons_dict = defaultdict(list)
