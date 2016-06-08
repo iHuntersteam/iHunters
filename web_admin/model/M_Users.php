5f8694b6b265ae7ebb507b02272848656c337173
Kharley Dmitriy <lippogoo@gmail.com>
2016-06-08 10:30:51 +0300

    Довлена функция хэширования пароля

--- web_admin/model/M_Users.php
+++ web_admin/model/M_Users.php
@@ -5,6 +5,7 @@ class M_Users
 	private static $instance;
 	private $msql;
 
+
 	public static function getInstance()
 	{
 		if(self::$instance==null)
@@ -17,24 +18,7 @@ class M_Users
 	function __construct()
 	{
 		$this->msql=M_Mysql::getInstance();
-	}
-
-	public function register($username,$password,$password_comfirm,$email)
-	{
-		$username=trim($username);
-		$password=trim($password);
-		$email=trim($email);
-		$check_username=$this->getByLogin($username);
-
-		if($password !== $password_comfirm) return false;
-		if($check_username==true) return false;
-
-
-		$table="users";
-		$object=["username"=>$username,"password"=>md5($password),'email'=>$email];
-		$this->msql->Insert($table,$object);
-
-		return true;
+	
 	}
 	public function getByLogin($username)
 	{
@@ -42,28 +26,46 @@ class M_Users
 		$rows=$this->msql->Select($query);
 		return $rows[0];
 	}
+
+	public function register($username,$password,$password_comfirm,$email)
+	{
+		$username=trim($username);
+		$password=trim($password);
+		$password=$this->generateHash($password);
+		$email=trim($email);
+		$check_username=$this->getByLogin($username);
+		if($password !== $password_comfirm) return false;
+		if($check_username==true) return false;
+		
+		$table="users";
+		$object=["username"=>$username,"password"=>$password,'email'=>$email];
+		$this->msql->Insert($table,$object);
+
+		return true;
+	}
+	
 	public function login($username,$password,$remember=true)
 	{
 		$user=$this->getByLogin($username);
 		
 		if($user==null) return false;
-
 		if($user['username']!=$username) return false;
-		if($user['password']!=md5($password)) return false;
+		if(!password_verify($password,$user['password'])) return false;
 
 		if($remember)
 		{
 			$expire = time() + 3600 * 24 * 10;
 			setcookie('username', $username, $expire);
-			setcookie('password', md5($password), $expire);
+			setcookie('password', $password, $expire);
 		}
 		return true;	
 	}
+	public function generateHash($password)
+	{
+		$option=['cost'=>10];
+		$password=password_hash($password,PASSWORD_BCRYPT,$option);
+		return $password;
+	}
 }
 
-
-
-
-
-
- ?>
+?>
