5c566483777810324e71dadd609e31792fab8f7d
pirr <tattoo1mag@gmail.com>
2016-06-08 15:15:47 +0800

    update

--- crawler/crawler_python/test_conn.py
+++ crawler/crawler_python/test_conn.py
@@ -1,10 +1,11 @@
-from db_connect import CrawlerSitesConnector
-from db_connect import CONN
+from db_connect import CrawlerSitesConnector, CrawlerHandlerConnector
 
 # получить несканированные страницы
 sites_con = CrawlerSitesConnector()
 if sites_con.need_scan():
     pages = sites_con.get_not_scan_pages_gen()
+    # тут получение персон и работа краулера
     print(pages)
-# пока нет коммита last_scan_pages в handler не обновится
-CONN.commit()
+
+    # обновление даты последнего сканирования в handler
+    CrawlerHandlerConnector.update_last_scan_pages()
