b0c4380ce4af6fc32e5fab3e4587999c6d5e0043
pirr <tattoo1mag@gmail.com>
2016-06-08 13:28:51 +0800

    update after refactoring db_connect.py (look previous commit)

--- crawler/crawler_python/test_conn.py
+++ crawler/crawler_python/test_conn.py
@@ -1,9 +1,10 @@
 from db_connect import CrawlerSitesConnector
 from db_connect import CONN
 
-#получить несканированные страницы
+# получить несканированные страницы
 sites_con = CrawlerSitesConnector()
-pages = sites_con.get_not_scan_pages_gen()
-print(pages)
-#пока нет коммита last_scan_pages не обновится 
-#CONN.commit()
+if sites_con.need_scan():
+    pages = sites_con.get_not_scan_pages_gen()
+    print(pages)
+# пока нет коммита last_scan_pages в handler не обновится
+CONN.commit()
