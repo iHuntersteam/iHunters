5c61e5ebd6a8bf51a17e2127fd413991d2bcb56d
Aleksej Smaga <tattoo1mag@gmail.com>
2016-06-30 22:38:06 +0800

    regexpWord refactor, return array of PatternStrings (structure) now

--- crawler/crawler_golang/parser.go
+++ crawler/crawler_golang/parser.go
@@ -10,10 +10,24 @@ import (
 )
 
 
+type PatternStrings struct {
+	person_id int
+	search_patterns []*regexp.Regexp
+}
 
-func regexpWord(word string) *regexp.Regexp {
+
+func regexpWord(persons map[int][]string) []PatternStrings {
 	// return regexp for words in person
-	return regexp.MustCompile(`(?im)(?:\A|\z|\s|[[:graph:]])` + word + `(?:[[:graph:]]|\A|\z|\s)`)
+	patternStrArray := []PatternStrings{}
+	for id, words := range persons {
+		patterns := []*regexp.Regexp{}
+		for _, word := range words {
+			pat := regexp.MustCompile(`(?im)(?:\A|\z|\s|[[:graph:]])` + word + `(?:[[:graph:]]|\A|\z|\s)`) 
+			patterns = append(patterns, pat)
+		}
+		patternStrArray = append(patternStrArray, PatternStrings{id, patterns})
+	}
+	return patternStrArray
 }
 
 func Parse(html_page []byte, persons map[int][]string) map[int]int {
