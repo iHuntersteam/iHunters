849a2cc79bbe4d0768738b2bcf52a45e556f7c50
Aleksej Smaga <tattoo1mag@gmail.com>
2016-07-03 16:28:31 +0800

    minor changes

--- crawler/crawler_golang/gethtml.go
+++ crawler/crawler_golang/gethtml.go
@@ -6,14 +6,13 @@ import (
 	"net/http"
 )
 
-
-func Crawl(url string) []byte {
+func Crawl(url string) (bytes []byte) {
 	//get html page (bytes)
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Panic("ERROR:", err)
 	}
 	defer resp.Body.Close()
-	bytes, _ := ioutil.ReadAll(resp.Body)
-	return bytes
+	bytes, _ = ioutil.ReadAll(resp.Body)
+	return
 }
